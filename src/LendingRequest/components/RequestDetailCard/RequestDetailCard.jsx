0626aaa071c95fb2ced8aa3b2cd5f1eaa5217cb9
Thanakrit Choutkhajung <oulespaul@gmail.com>
2021-07-01 09:52:55 +0700

    feat: add validator in lending-request

--- src/LendingRequest/components/RequestDetailCard/RequestDetailCard.jsx
+++ src/LendingRequest/components/RequestDetailCard/RequestDetailCard.jsx
@@ -5,10 +5,10 @@ import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -41,7 +41,7 @@ export const useStyles = makeStyles(() => ({
 const RequestDetailCard = (props) => {
   const classes = useStyles();
   const { handleValue, handleSubmit, plant } = props;
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState("");
 
   const handleSliderChange = (event, newValue) => {
     setValue(newValue);
@@ -53,58 +53,66 @@ const RequestDetailCard = (props) => {
     handleValue(value);
   };
 
+  const handleSubmitRequest = () => {
+    handleSubmit();
+  };
+
   return (
     <Card className={classes.cardRequestDetail}>
-      <CardContent>
-        <Grid container spacing={4}>
-          <Grid item md={12}>
-            <Typography variant="h5">รายละเอียดขอเช่า</Typography>
-          </Grid>
+      <ValidatorForm onSubmit={handleSubmitRequest}>
+        <CardContent>
+          <Grid container spacing={4}>
+            <Grid item md={12}>
+              <Typography variant="h5">รายละเอียดขอเช่า</Typography>
+            </Grid>
 
-          <Grid item md={12}>
-            <Typography>ปริมาณไฟฟ้าที่ต้องการ</Typography>
-          </Grid>
+            <Grid item md={12}>
+              <Typography>ปริมาณไฟฟ้าที่ต้องการ</Typography>
+            </Grid>
 
-          <Grid item container direction="column" alignItems="center">
-            <TextField
-              id="filled-basic"
-              size="medium"
-              variant="filled"
-              value={value}
-              style={{ textAlign: "center" }}
-              onChange={handleInputChange}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">MW</InputAdornment>
-                ),
-              }}
-            />
-
-            <div className={classes.slider}>
-              <Slider
-                value={typeof value === "number" ? value : 0}
-                aria-labelledby="discrete-slider"
-                valueLabelDisplay="auto"
-                step={100}
-                marks
-                min={plant.volumeBalance / 10}
-                max={plant.volumeBalance}
-                onChange={handleSliderChange}
+            <Grid item container direction="column" alignItems="center">
+              <TextValidator
+                id="filled-basic"
+                size="medium"
+                variant="filled"
+                value={value}
+                style={{ textAlign: "center" }}
+                onChange={handleInputChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">MW</InputAdornment>
+                  ),
+                }}
+                validators={["required"]}
+                errorMessages={["กรุณากรอกปริมาณไฟฟ้าที่ต้องการ"]}
               />
-            </div>
-          </Grid>
-        </Grid>
-      </CardContent>
 
-      <CardActions className={classes.cardActions}>
-        <Button
-          variant="contained"
-          className={classes.requestSubmit}
-          onClick={handleSubmit}
-        >
-          ส่งคำร้องขอ
-        </Button>
-      </CardActions>
+              <div className={classes.slider}>
+                <Slider
+                  value={typeof value === "number" ? value : 0}
+                  aria-labelledby="discrete-slider"
+                  valueLabelDisplay="auto"
+                  step={100}
+                  marks
+                  min={plant.volumeBalance / 10}
+                  max={plant.volumeBalance}
+                  onChange={handleSliderChange}
+                />
+              </div>
+            </Grid>
+          </Grid>
+        </CardContent>
+
+        <CardActions className={classes.cardActions}>
+          <Button
+            variant="contained"
+            className={classes.requestSubmit}
+            type="submit"
+          >
+            ส่งคำร้องขอ
+          </Button>
+        </CardActions>
+      </ValidatorForm>
     </Card>
   );
 };
