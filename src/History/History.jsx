60f1b56865e3bab6b8f7e042ae019d2bdc54ce60
Thanakrit Choutkhajung <oulespaul@gmail.com>
2021-07-01 17:45:42 +0700

    fix: disable dependency useEffect

--- src/History/History.jsx
+++ src/History/History.jsx
@@ -19,6 +19,7 @@ const History = () => {
   useEffect(() => {
     fetchPlantInUser();
     fetchHistory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchPlantInUser = async () => {
