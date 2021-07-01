cdec3361fdf26fb8cec4f1cae4ce68ab9a81ab5f
Thanakrit Choutkhajung <oulespaul@gmail.com>
2021-07-01 10:19:22 +0700

    feat: add font in app

--- src/App.jsx
+++ src/App.jsx
@@ -10,43 +10,57 @@ import { Profile } from "./Profile";
 import { LendingOffer } from "./LendingOffer";
 import { History } from "./History";
 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
 import ProtectedRoute from "shared/components/ProtectedRoute";
 
 import { AlertProvider } from "shared/context/alertContext";
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Kanit", sans-serif',
+  },
+});
+
 const App = () => {
   return (
-    <AlertProvider>
-      <Switch>
-        <Route path="/register" component={Register} />
+    <ThemeProvider theme={theme}>
+      <AlertProvider>
+        <Switch>
+          <Route path="/register" component={Register} />
 
-        <Route path="/login" component={Login} />
+          <Route path="/login" component={Login} />
 
-        <Route path="/app/:path?/:param1?" exact>
-          <AppLayoutRoute>
-            <Switch>
-              <ProtectedRoute exact path="/app/feed" component={LendingFeed} />
+          <Route path="/app/:path?/:param1?" exact>
+            <AppLayoutRoute>
+              <Switch>
+                <ProtectedRoute
+                  exact
+                  path="/app/feed"
+                  component={LendingFeed}
+                />
 
-              <ProtectedRoute path="/app/profile" component={Profile} />
+                <ProtectedRoute path="/app/profile" component={Profile} />
 
-              <ProtectedRoute
-                path="/app/lending-request/:plantId"
-                component={LendingRequest}
-              />
+                <ProtectedRoute
+                  path="/app/lending-request/:plantId"
+                  component={LendingRequest}
+                />
 
-              <ProtectedRoute
-                path="/app/lending-offer/:lendingId"
-                component={LendingOffer}
-              />
+                <ProtectedRoute
+                  path="/app/lending-offer/:lendingId"
+                  component={LendingOffer}
+                />
 
-              <ProtectedRoute path="/app/history" component={History} />
-            </Switch>
-          </AppLayoutRoute>
-        </Route>
+                <ProtectedRoute path="/app/history" component={History} />
+              </Switch>
+            </AppLayoutRoute>
+          </Route>
 
-        <Redirect to="/app/feed" />
-      </Switch>
-    </AlertProvider>
+          <Redirect to="/app/feed" />
+        </Switch>
+      </AlertProvider>
+    </ThemeProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import "./app.css";
 
 ReactDOM.render(
   <Router>
