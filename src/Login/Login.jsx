0b0583c522c5405613b15641eba63e8a9a4abf25
Thanakrit Choutkhajung <oulespaul@gmail.com>
2021-07-01 10:36:01 +0700

    feat: add alert when login failed

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,12 +7,15 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
+import AlertBox from "../shared/components/AlertBox";
+
 import { useStyles } from "./login.styles";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import AuthService from "shared/services/authService";
 import handlePromise from "shared/handlePromise";
+import { AlertType, useAlert } from "shared/context/alertContext";
 
 function SignInSide() {
   const classes = useStyles();
@@ -22,6 +25,7 @@ function SignInSide() {
     password: "",
   };
   const [credentials, setCredential] = useState(initCredential);
+  const { dispatch } = useAlert();
 
   const handleFormChange = (event) => {
     setCredential({
@@ -36,79 +40,86 @@ function SignInSide() {
     const [, error] = await handlePromise(AuthService.signIn(credentials));
 
     if (error) {
-      return alert(error);
+      return dispatch({
+        type: AlertType.ERROR,
+        payload: { message: "username หรือ password ไม่ถูกต้อง" },
+      });
     }
 
     return history.push("/app/feed");
   };
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
+    <>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
 
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
 
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Typography component="h1" variant="h2">
-            Smart Energy
-          </Typography>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h2">
+              Smart Energy
+            </Typography>
 
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              value={credentials.username}
-              autoComplete="username"
-              onChange={handleFormChange}
-              autoFocus
-            />
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                value={credentials.username}
+                autoComplete="username"
+                onChange={handleFormChange}
+                autoFocus
+              />
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              value={credentials.password}
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={handleFormChange}
-            />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                value={credentials.password}
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleFormChange}
+              />
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={submitLogin}
-            >
-              เข้าสู่ระบบ
-            </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={submitLogin}
+              >
+                เข้าสู่ระบบ
+              </Button>
 
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link
-                  href="/register"
-                  variant="body2"
-                  className={classes.registerText}
-                >
-                  {"สมัครสมาชิก"}
-                </Link>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link
+                    href="/register"
+                    variant="body2"
+                    className={classes.registerText}
+                  >
+                    {"สมัครสมาชิก"}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-          </form>
-        </div>
+            </form>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
+
+      <AlertBox />
+    </>
   );
 }
 
